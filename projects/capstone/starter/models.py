e13a00fbcf6e73201dd13b004bba56d461818520
kriakria <kigi@novonordisk.com>
2021-01-14 18:10:52 +0100

    Models.py created with Movie, Actor and Cast tables.
    Tables Movie, Actor and Cast created, and setup_db.
    db_user and db_password retrieved from .env.

--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,1 +1,71 @@
+import os
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from flask_sqlalchemy import SQLAlchemy
+import json
 
+db_name = 'capstone'
+
+# get database user and password from env file
+db_user = os.environ['DB_USER']
+db_password = os.environ['DB_PASSWORD']
+
+# set the database path
+db_path = "postgres://{}:{}@{}/{}".format(db_user, db_password, 'localhost:5432', db_name)
+
+db = SQLAlchemy()
+
+def setup_db(app, database_path=db_path):
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_path
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+    # db.create_all()
+
+
+'''
+Movie
+'''
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    release_date = Column(DateTime)
+
+    def __repr__ (self):
+        return f'<Movie id {self.id}, Movie title {self.title}, Movie release date {self.release_date}>'
+
+
+'''
+Actor
+'''
+
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    age = Column(Integer)
+    gender = Column(String)
+
+    def __repr__ (self):
+        return f'<Actor id {self.id}, Actor name {self.name}, Actor age {self.age}, Actor gender {self.gender}>'
+
+
+'''
+Cast
+'''
+
+
+class Cast(db.Model):
+    __tablename__ = 'casts'
+
+    id = Column(Integer, primary_key=True)
+    movie_id = Column(Integer, ForeignKey('movies.id'), nullable=False)
+    actor_id = Column(Integer, ForeignKey('actors.id'), nullable=False)
+
+    def __repr__ (self):
+        return f'<Cast id {self.id}, movie_id {self.movie_id}, actor_id {self.actor_id}>'
